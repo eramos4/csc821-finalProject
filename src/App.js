2fc9939fee7b0311638a97b575ac9ac86a127540
Eduardo Ramos <ramosedward8@gmail.com>
2020-04-21 23:23:41 -0700

    file upload system

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import FileUpload from './components/FileUpload.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Upload An Image</h1>
+     <FileUpload></FileUpload>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/components/FileUpload.js
@@ -0,0 +1,55 @@
+import React, { Fragment, useState } from 'react';
+import axios from 'axios';
+import './FileUpload.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const FileUpload = () => {
+    const [file, setFile] = useState('');
+    const [filename, setFilename] = useState('Choose File');
+    
+    const fileSelectedHandler = (event) =>{
+        
+        setFile(event.target.files[0]);
+        setFilename(event.target.files[0].name);
+    }
+
+    const fileUploadHander = (event) =>{
+        event.preventDefault();
+        const formData = new FormData();
+        formData.append('file', file);
+        axios.post('./uploads', formData)
+        .then(
+            res => {
+                console.log(res);
+            });
+    }
+
+    return(
+        <div className="holding">
+            <div className="file-upload">
+                <div className="custom-file">
+                    <input 
+                    type="file"
+                    className="custom-file-input"
+                    id="inputGroupFile01"
+                    aria-describedby="inputGroupFileAddon01"
+                    onChange={fileSelectedHandler}
+                    />
+                    <label className="custom-file-label" htmlFor="inputGroupFile01">
+                    {filename}
+                    </label>
+                </div>
+            </div>
+
+            <div className="">
+                <button className="upload"  onClick={fileUploadHander}>
+                    Upload
+                </button>
+            </div>
+
+        </div>
+
+    );
+};
+
+export default FileUpload;
