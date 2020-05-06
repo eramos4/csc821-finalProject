02ff4e741ee87753da3bfd98762c6c6741d972ca
Eduardo Ramos <ramosedward8@gmail.com>
2020-05-05 21:59:43 -0700

    added progess bar and minor fixes

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -2,10 +2,12 @@ import React, {useState } from 'react';
 import axios from 'axios';
 import './FileUpload.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Progress from './Progress';
 
 const FileUpload = () => {
     const [file, setFile] = useState('');
     const [filename, setFilename] = useState('Choose File');
+    const [uploadPercentage, setUploadPercentage] = useState(0);
     
     const fileSelectedHandler = (event) =>{
         
@@ -17,16 +19,25 @@ const FileUpload = () => {
         event.preventDefault();
         const formData = new FormData();
         formData.append('file', file);
-        axios.post('/upload', formData)
+        axios.post('/upload', formData,config)
         .then(
             res => {
                 console.log(res);
             });
     }
 
-    const saveFile = (event) =>{
-        event.preventDefault();
-        
+    const config = {
+        onUploadProgress: progressEvent => {
+            setUploadPercentage(
+              parseInt(
+                Math.round((progressEvent.loaded * 100) / progressEvent.total)
+              )
+            );
+           
+  
+            // Clear percentage
+            setTimeout(() => setUploadPercentage(0), 10000);
+          }
     }
 
     return(
@@ -43,6 +54,10 @@ const FileUpload = () => {
                     <label className="custom-file-label" htmlFor="inputGroupFile01">
                     {filename}
                     </label>
+
+                    
+                    <Progress percentage={uploadPercentage} />
+
                 </div>
             </div>
 
